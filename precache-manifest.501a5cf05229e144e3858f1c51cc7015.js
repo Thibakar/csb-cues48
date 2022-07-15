0e15252f877b4d13d2fde183f1546dd9ebfbf0b5
Code Sandbox <hello@codesandbox.io>
2022-07-15 09:13:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.501a5cf05229e144e3858f1c51cc7015.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925830ef378ac9e42a0a9993415a35f2",
+    "url": "https://thibakar.github.io/csb-cues48/index.html"
+  },
+  {
+    "revision": "c1b57d5b92d1ea32e7fd",
+    "url": "https://thibakar.github.io/csb-cues48/static/css/main.bec89ba6.chunk.css"
+  },
+  {
+    "revision": "7b0407fec87c625ff227",
+    "url": "https://thibakar.github.io/csb-cues48/static/js/2.5a45f145.chunk.js"
+  },
+  {
+    "revision": "d7e164bc880d25710db675f58f847190",
+    "url": "https://thibakar.github.io/csb-cues48/static/js/2.5a45f145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2819bb3f4818fe9b617",
+    "url": "https://thibakar.github.io/csb-cues48/static/js/3.8d6f3952.chunk.js"
+  },
+  {
+    "revision": "3b0baed8d445fb476a4f",
+    "url": "https://thibakar.github.io/csb-cues48/static/js/4.b8fcfe1e.chunk.js"
+  },
+  {
+    "revision": "c1b57d5b92d1ea32e7fd",
+    "url": "https://thibakar.github.io/csb-cues48/static/js/main.88ba07c8.chunk.js"
+  },
+  {
+    "revision": "ee6ac93345df225df8d3",
+    "url": "https://thibakar.github.io/csb-cues48/static/js/runtime-main.960b9cfd.js"
+  },
+  {
+    "revision": "a77f19fe7f86b49a8d3b2471c3e304ee",
+    "url": "https://thibakar.github.io/csb-cues48/static/media/Basix_1400x.a77f19fe.jpg"
+  },
+  {
+    "revision": "284a686314a5c85fbc87f914ea06ab4b",
+    "url": "https://thibakar.github.io/csb-cues48/static/media/Basix_700.284a6863.jpg"
+  },
+  {
+    "revision": "344017e842b823214fc40fc36a1e109a",
+    "url": "https://thibakar.github.io/csb-cues48/static/media/Basix_900.344017e8.jpg"
+  },
+  {
+    "revision": "581f2b627a45b4ed584e0fbbece43b75",
+    "url": "https://thibakar.github.io/csb-cues48/static/media/essentials_1400x.581f2b62.jpg"
+  },
+  {
+    "revision": "f2d6f55fe1dd8d8e528a950f97f059cd",
+    "url": "https://thibakar.github.io/csb-cues48/static/media/mainvideo2.f2d6f55f.mp4"
+  }
+]);
